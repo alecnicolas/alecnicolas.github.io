e1a4b8afa1d33f39fd3c3eea262f8c59dc05a6b5
alecnicolas <alecnmarcum@gmail.com>
2019-03-27 23:43:42 -0700

    sorted by priority

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
-      <h1>Hello</h1>
-  </div>
+	<div id="app">
+		<div class="container">
+            <h1>Highlighter</h1>
+			<highlighter></highlighter>
+		</div>
+	</div>
 </template>
 
 <script>
-import Highlighter from './components/Highlighter.vue'
+import Highlighter from "./components/Highlighter.vue";
 export default {
-  name: 'app',
-}
+	name: "app",
+	components: {
+		Highlighter
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/components/Highlighter.vue
+++ src/components/Highlighter.vue
@@ -1,12 +1,58 @@
 <template>
-    
+	<div class="row justify-content-center">
+		<div class="col-8">
+			<div class="input-group">
+				<input type="text" class="form-control" :model="inputString">
+				<button type="button" class="btn btn-primary" @click.prevent="highlightText">Highlight!</button>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            string: ""
+	data() {
+		return {
+			inputString: "abcdefgh",
+			highlights: [
+                {
+                    startOffset: 1,
+                    endOffset: 4,
+                    color: 'blue',
+                    priority: 3
+                },
+                {
+                    startOffset: 0,
+                    endOffset: 2,
+                    color: 'red',
+                    priority: 8
+                },
+                {
+                    startOffset: 3,
+                    endOffset: 6,
+                    color: 'yellow',
+                    priority: 0
+                }
+            ]
+		};
+	},
+	methods: {
+        highlightText() {
+            this.sortPriority()
+            let result = ""
+
+            for (let i=0; i<this.highlights.length; i++){
+                let highlight = this.highlights[i]
+                result += this.inputString.substring(highlight.startOffset, highlight.endOffset)
+            }
+
+            console.log(result)
+        },
+
+        sortPriority() {
+            this.highlights.sort(function(first, second){
+                return first.priority - second.priority
+            })
         }
     }
-}
+};
 </script>
